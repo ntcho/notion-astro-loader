f421fdf7739ea3ccac38cad6c47c34a1096a0bca
Nathan Cho <ntcho13@gmail.com>
2025-06-05 23:08:47 -0400

    chore: update exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
-export * from './format.js';
 export { type NotionLoaderOptions, notionLoader } from './loader.js';
 export { notionPageSchema } from './schemas/page.js';
+export * from './format.js';
+export * from './types.js';
